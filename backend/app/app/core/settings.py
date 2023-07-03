5530cecfb699a2db65d21f9ece0a0f4d5cfcd07d
Axeratos <nikitalukianitcares@gmail.com>
2023-07-03 10:34:53 +0300

    Settings file created

---
+++ backend/app/app/core/settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+
+env_path = Path(__file__).resolve().parents[2].joinpath(".env")
+print(env_path)
+
+
+class Settings(BaseSettings):
+    DB_URL: str
+
+    class Config:
+        env_file = env_path
+
+
+app_config = Settings()
