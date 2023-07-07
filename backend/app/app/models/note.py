4c503fee71112f4d3891057b80f940e87cff7683
Axeratos <nikitalukianitcares@gmail.com>
2023-07-07 10:51:04 +0300

    Relationships between models set

--- backend/app/app/models/note.py
+++ backend/app/app/models/note.py
@@ -1,8 +1,12 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from app.db import Base
+from app.models import User
 
 
 class Note(Base):
+    __tablename__ = "note"
     text: Mapped[str]
-
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    owner: Mapped[User] = relationship(back_populates="tasks")
--- backend/app/app/models/task.py
+++ backend/app/app/models/task.py
@@ -1,13 +1,17 @@
 from datetime import date, time
 from typing import Optional
 
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
+from app.models import User
 
 
 class Task(Base):
+    __tablename__ = "task"
     title: Mapped[str] = mapped_column(String(100))
     deadline_date: Mapped[date]
     deadline_time: Mapped[Optional[time]]
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    owner: Mapped[User] = relationship(back_populates="tasks")
