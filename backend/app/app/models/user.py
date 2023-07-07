25e30e39cb11f07f46f5ceffb8455d06a23d4ce4
Axeratos <nikitalukianitcares@gmail.com>
2023-07-07 10:50:48 +0300

    User model defined

---
+++ backend/app/app/models/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, validates, mapped_column
+
+from app.db import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+    email: Mapped[str] = mapped_column(String, unique=True)
+    username: Mapped[str] = mapped_column(String, unique=True)
+    password: Mapped[str]
+    is_verified: Mapped[bool] = mapped_column(default=True)
+
+    @validates("password")
+    def validate_password(self, key, password):
+        if 8 > len(password):
+            raise ValueError("password is too short")
+        return password
