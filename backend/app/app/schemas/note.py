4d17c08097841c88e315def17f313e63bf49a19b
Axeratos <nikitalukianitcares@gmail.com>
2023-07-09 11:09:17 +0300

    Note pydantic schemas created

---
+++ backend/app/app/schemas/note.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+from .base import BaseDBSchema
+
+
+class NoteBase(BaseModel):
+    text: str | None
+
+
+class NoteCreate(NoteBase):
+    text: str
+
+
+class NoteUpdate(NoteBase):
+    pass
+
+
+class NoteDB(NoteBase, BaseDBSchema):
+    pk: int
+    text: str
