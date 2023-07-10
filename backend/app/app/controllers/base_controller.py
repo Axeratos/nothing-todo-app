be47bc91212fd2769f220ecfd315dae88d9dbc93
Axeratos <nikitalukianitcares@gmail.com>
2023-07-10 11:26:12 +0300

    Methods implementations and new methods in base controller

--- backend/app/app/controllers/base_controller.py
+++ backend/app/app/controllers/base_controller.py
@@ -1,3 +1,5 @@
+from pydantic import BaseModel
+from sqlalchemy import select, or_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import Base
@@ -9,10 +11,24 @@ class BaseDatabaseController:
         self.session = session
 
     async def get(self, **kwargs):
+        queryset = await self.session.scalars(select(self.model).filter_by(**kwargs))
+        return queryset.first()
+
+    async def get_all(self):
         pass
 
-    async def create(self, data):
-        pass
+    async def get_with_options(self, **kwargs):
+        queryset = await self.session.scalars(select(self.model).where(or_(**kwargs)))
+        return queryset.first()
+
+    async def create(self, data: dict | BaseModel):
+        if isinstance(data, BaseModel):
+            data = data.dict()
+        new_object = self.model(**data)
+        self.session.add(new_object)
+        await self.session.flush()
+        await self.session.refresh(new_object)
+        return new_object
 
     async def update(self, data):
         pass
