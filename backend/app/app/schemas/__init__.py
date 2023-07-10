d589789ac0014b6a3ad6ae60b0281846255e0ee8
Axeratos <nikitalukianitcares@gmail.com>
2023-07-10 10:50:43 +0300

    User pydantic schemas created

---
+++ backend/app/app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .user import UserCreate, UserUpdate, UserDB
+from .task import TaskCreate, TaskUpdate, TaskDB
+from .note import NoteCreate, NoteUpdate, NoteDB
---
+++ backend/app/app/schemas/user.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr
+
+from .base import BaseDBSchema
+
+
+class UserBase(BaseModel):
+    email: EmailStr | None
+    username: str | None
+    password: str | None
+
+
+class UserCreate(UserBase):
+    email: EmailStr
+    username: str
+    password: str
+
+
+class UserUpdate(UserBase):
+    pass
+
+
+class UserDB(UserBase, BaseDBSchema):
+    email: EmailStr
+    username: str
