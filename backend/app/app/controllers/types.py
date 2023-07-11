864ab5453677f37e9b9b89c072a7547ad1e1e376
Axeratos <nikitalukianitcares@gmail.com>
2023-07-11 10:23:54 +0300

    Types for controllers created

---
+++ backend/app/app/controllers/types.py
@@ -0,0 +1,9 @@
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+from app.models.base_model import Base
+
+ModelType = TypeVar("ModelType", bound=Base)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
