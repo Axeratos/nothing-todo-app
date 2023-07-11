25cc26d11f42ea3d01906b8f26eeb83db7fb8215
Axeratos <nikitalukianitcares@gmail.com>
2023-07-11 09:53:52 +0300

    Including routes into main router

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,3 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from app.api.api_v1.api_register import api_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
