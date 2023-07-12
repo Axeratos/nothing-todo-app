deebc164d9ecc01e4252a676cf20e5333de977eb
Axeratos <nikitalukianitcares@gmail.com>
2023-07-12 10:33:58 +0300

    Login endpoint registered

--- backend/app/app/api/api_v1/api_register.py
+++ backend/app/app/api/api_v1/api_register.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import user
+from app.api.api_v1.endpoints import user, login
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/user", tags=["user"])
+api_router.include_router(login.router, tags=["login"])
