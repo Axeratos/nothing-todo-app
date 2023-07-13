ebcf7b77986f76c8f15b833dd3fbb45b632a5593
Axeratos <nikitalukianitcares@gmail.com>
2023-07-13 11:50:19 +0300

    Current verified user annotation created

--- backend/app/app/services/annotations.py
+++ backend/app/app/services/annotations.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.dependencies import get_session
+from app.api.dependencies import get_session, get_current_verified_user
+from app.schemas import UserDB
 
 DBSession = Annotated[AsyncSession, Depends(get_session)]
+CurrentVerifiedUser = Annotated[UserDB, Depends(get_current_verified_user)]
