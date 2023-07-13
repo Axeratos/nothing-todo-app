1d6ba3bf2146353d83fe3fb593926fae7bdf24bf
Axeratos <nikitalukianitcares@gmail.com>
2023-07-13 11:51:13 +0300

    Base controller changed on user

--- backend/app/app/api/dependencies.py
+++ backend/app/app/api/dependencies.py
@@ -1,9 +1,9 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from starlette import status
 
-from app.controllers import BaseDatabaseController
+from app.controllers import UserDatabaseController
 from app.core.security import decode_token
 from app.db.database_core import engine
 from app.models import User
@@ -21,16 +21,16 @@ async def get_session():
             raise e
 
 
-async def get_current_user(session: Depends(get_session), token: str = Depends(oauth2)):
+async def get_current_user(session: Depends(get_session), token: str = Depends(oauth2)) -> User:
     token_data = decode_token(token)
-    controller = BaseDatabaseController(User, session)
-    user = controller.get(id=token_data.subject)
+    controller = UserDatabaseController(User, session)
+    user = await controller.get(id=token_data.subject)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
 
-def get_current_verified_user(current_user: User = Depends(get_current_user)):
+def get_current_verified_user(current_user: User = Depends(get_current_user)) -> User:
     if not current_user.is_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
