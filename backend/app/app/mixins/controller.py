6f938f3045a2e4dc3ab7b999ff64dc67d19fcf47
Axeratos <nikitalukianitcares@gmail.com>
2023-07-14 11:35:49 +0300

    Mixin for managing owner related data created

---
+++ backend/app/app/mixins/controller.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+from app.controllers import BaseDatabaseController
+from app.types.controller import CreateSchemaType, ModelType
+
+
+class OwnerOperationsMixin:
+    async def create_with_owner(self: BaseDatabaseController, data: dict | CreateSchemaType, pk: int) -> ModelType:
+        if isinstance(data, BaseModel):
+            data = data.dict()
+        new_object = self.model(**data, owner_id=pk)
+        self.session.add(new_object)
+        await self.session.flush()
+        await self.session.commit()
+        await self.session.refresh(new_object)
+        return new_object
