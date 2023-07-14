9f6b97e1a944190e56b46958fdf9d0e80e126481
Axeratos <nikitalukianitcares@gmail.com>
2023-07-14 11:35:22 +0300

    Annotations changed

--- backend/app/app/services/annotations.py
+++ backend/app/app/services/annotations.py
@@ -4,7 +4,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.dependencies import get_session, get_current_verified_user
-from app.schemas import UserDB
+from app.models import User
 
 DBSession = Annotated[AsyncSession, Depends(get_session)]
-CurrentVerifiedUser = Annotated[UserDB, Depends(get_current_verified_user)]
+CurrentVerifiedUser = Annotated[User, Depends(get_current_verified_user)]
