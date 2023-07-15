8ba88cb8e7b44c12dcbdffb8e572790a263a38a6
Axeratos <nikitalukianitcares@gmail.com>
2023-07-15 10:27:01 +0300

    Note controller subclasses owner operations mixin class

--- backend/app/app/controllers/note_controller.py
+++ backend/app/app/controllers/note_controller.py
@@ -1,7 +1,8 @@
+from app.mixins.controller import OwnerOperationsMixin
 from app.models import Note
 from app.schemas import NoteCreate, NoteUpdate
 from .base_controller import BaseDatabaseController
 
 
-class NoteDatabaseController(BaseDatabaseController[Note, NoteCreate, NoteUpdate]):
+class NoteDatabaseController(BaseDatabaseController[Note, NoteCreate, NoteUpdate], OwnerOperationsMixin):
     pass
