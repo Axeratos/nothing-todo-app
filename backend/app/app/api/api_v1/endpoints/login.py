267823c37da1af68c97b45f75cfcc85fe9c5739e
Axeratos <nikitalukianitcares@gmail.com>
2023-07-15 10:27:26 +0300

    Authentication flow corrected for now

--- backend/app/app/api/api_v1/endpoints/login.py
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -1,20 +1,21 @@
 from datetime import timedelta
+from typing import Annotated
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.controllers import BaseDatabaseController
 from app.core.security import verify_password, create_token
 from app.models import User
-from app.schemas import UserLogin
 from app.services.annotations import DBSession
 
 router = APIRouter()
 
 
 @router.post("/access-token")
-async def login_access_token(login_data: UserLogin, session: DBSession):
+async def login_access_token(login_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: DBSession):
     controller = BaseDatabaseController(User, session)
-    queryset = await controller.get_options_queryset(email=login_data.login, username=login_data.login)
+    queryset = await controller.get_options_queryset(email=login_data.username, username=login_data.username)
     user = queryset.first()
     if not user:
         raise HTTPException(status_code=404, detail={"field": "username", "msg": "User does not exists"})
